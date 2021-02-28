7cfe7bd92b38c6f61d483bd0dbee697727920449
Rutuj Bhakre <bhakrerutuj@gmail.com>
2021-02-28 17:57:38 +0530

    Done with bubble sort and animations

---
+++ js_files/bubble.js
@@ -0,0 +1,71 @@
+async function Bubble()
+    {
+      var f=0;
+      console.log(array);	
+      for(var i=0;i<100;i++)
+      {
+      	for(var j=0;j<100-i-1;j++)
+      	{
+      		
+            arr1= document.getElementById(j);
+            arr2=document.getElementById(j+1);
+            //console.log(arr1.style.height,arr2.style.height);
+            if(array[j] > array[j+1])
+            {
+
+                 f=1;
+                //console.log(array[j],array[j+1]);
+                //console.log(arr1,arr2)
+                style1 = window.getComputedStyle(arr1);
+                style2 = window.getComputedStyle(arr2);
+
+                const transform1 = style1.getPropertyValue("height");
+                const transform2 = style2.getPropertyValue("height");
+               
+                // console.log(transform1);  
+                //   console.log(transform2);
+                
+                var temp=array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
+                //onsole.log(style1.height);
+                //console.log(style2.height);
+                 
+                 arr1.style.background = "red";
+                 arr2.style.background="red";
+                 
+
+                 temp=arr1.style.height;
+                 arr1.style.height = arr2.style.height;
+                 arr2.style.height=temp;
+
+                  const fast = await resolveAfter1Second()
+                  //console.log(fast)
+                 //  await 2;
+                       arr1.style.background = "white";
+                  arr2.style.background="white";
+            
+        }
+         
+    }
+    if(f==1){
+      arr1.style.background = "green";
+                  arr2.style.background="green";
+                  f=0;
+    }
+    console.log(array);
+  }    
+    
+
+}
+
+      
+function resolveAfter1Second() {
+  //console.log("starting fast promise")
+  return new Promise(resolve => {
+    setTimeout(function() {
+      resolve("fast")
+      //console.log("fast promise is done")
+    }, 50)
+  })
+}
--- js_files/main.js
+++ js_files/main.js
@@ -6,8 +6,7 @@ function getRandomNumber(){
 
 function Bars(){
 
-            let lineContainerElement = document.getElementById('heightBars')
-
+    let lineContainerElement = document.getElementById('heightBars')
             // Remove Present Children
             let child = lineContainerElement.lastElementChild;  
             while (child) { 
@@ -23,6 +22,7 @@ function Bars(){
 
                 let lineElement = document.createElement('div')
                 lineElement.className = "bar"
+                lineElement.id=i
                 lineElement.style.height = randomNumber + 'px'
                 lineElement.style.width = 100/100-(2*0.1) + "%"
                 lineContainerElement.appendChild(lineElement)
@@ -30,28 +30,3 @@ function Bars(){
             console.log(array);
             console.log(lineContainerElement);
         }
-	// var div_sizes=[];
- //    var divs=[];
- //    var margin_size;
- //    var array_size=100;
-	// var cont=document.getElementById("heightBars");
-	// cont.style="flex-direction:row";
-
-	// for(var i=0;i<100;i++)
- //    {
- //        cont.innerHTML="";
- //        div_sizes[i]=Math.floor(Math.random() * 100 ) + 1;
- //        //console.log(div_sizes);
- //        divs[i]=document.createElement("div");
- //        //console.log(divs);
- //        var text = document.createTextNode("Tutorix is the best e-learning platform");
- //        divs[i].appendChild(text);
- //        cont.appendChild(divs[i]);
- //        //console.log(cont);
- //        margin_size=0.1;
- //        divs[i].style=" margin:0% " + margin_size + "%; background-color:white; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
- //    }
- //    console.log(cont);
- //    console.log(div_sizes);
- //    console.log(divs);
-
